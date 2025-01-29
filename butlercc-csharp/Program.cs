529d9f4830a7ef03e3904c2f8747096718e8f1c7
aiitom <tsays92@yahoo.com>
2025-01-29 02:09:29 -0600

    String exercise using compare/equals operator

--- butlercc-csharp/Program.cs
+++ butlercc-csharp/Program.cs
@@ -1,4 +1,6 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
+using static System.String;
 class DemoVariables
 {
     static void Main()
@@ -6,8 +8,12 @@ class DemoVariables
         string name1 = "Amy";
         string name2 = "Amy";
         string name3 = "Matthew";
-        Console.WriteLine("compare {0} to {1}: {2}", name1, name2, name1 == name2);
-        Console.WriteLine("compare {0} to {1}: {2}", name1, name3, name1 == name3);
+        Console.WriteLine("{0} to {1}; Equals() method gives {2}", name1, name2, name1.Equals(name2));
+        Console.WriteLine("{0} to {1}; Equals() method gives {2}", name1, name3, name1.Equals(name3));
+        Console.WriteLine("{0} to {1}; CompareTo() method gives {2}", name1, name2, name1.CompareTo(name2));
+        Console.WriteLine("{0} to {1}; CompareTo() method gives {2}", name1, name2, name1.CompareTo(name3));
+        Console.WriteLine("{0} to {1}; Compare() method gives {2}", name1, name2, Compare(name1,name2));
+        Console.WriteLine("{0} to {1}; Compare() method gives {2}", name1, name2, Compare(name1,name3));
 
     }
 }
