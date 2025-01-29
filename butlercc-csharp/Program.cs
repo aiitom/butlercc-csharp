badf6d0e44d4ed2b307b95b74b378a3979d49b64
aiitom <tsays92@yahoo.com>
2025-01-28 18:46:37 -0600

    Signed Int and UnsignedInt exercise.

--- butlercc-csharp/Program.cs
+++ butlercc-csharp/Program.cs
@@ -1,13 +1,11 @@
 ﻿using static System.Console;
-class DisplaySomeMoney
+class DemoVariables
 {
     static void Main()
     {
-        WriteLine("{0, 5}", 4);
-        WriteLine("{0, 5}", 56);
-        WriteLine("{0, 5}", 789);
-        WriteLine("{0, -8}{1, -8}", "Richard", "Lee");
-        WriteLine("{0, -8}{1, -8}", "Marcia", "Parker");
-        WriteLine("{0, -8}{1, -8}", "Ed", "Tompkins");
+        int anInt = -123;
+        uint anUnsignedInt = 567;
+        WriteLine("The int is {0} and the unsigned int is {1}.",
+            anInt, anUnsignedInt);
     }
 }
