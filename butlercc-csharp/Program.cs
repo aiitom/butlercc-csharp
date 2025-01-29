bb3526ff8f541f92d376a172cb26bad3c584222c
aiitom <tsays92@yahoo.com>
2025-01-29 01:48:41 -0600

    Understanding Numeric Type Conversion #2

--- butlercc-csharp/Program.cs
+++ butlercc-csharp/Program.cs
@@ -3,9 +3,11 @@ class DemoVariables
 {
     static void Main()
     {
-        int hoursWorked = 36;
-        double payRate = 12.35;
-        double grossPay = hoursWorked * payRate;
-        Console.WriteLine("The total amount I made this pay period was {0}.", grossPay);
+        int score = 20;
+        double bankBalance = 345.67;
+        score  = (int)bankBalance + 2; //Was "5" but added a ".2" at the end to see it change from int to double.
+        //Did implicit by adding "(int)" and changing swaping identifiers and changing 5.2 to 2.
+        Console.WriteLine("score is {0}", score);
+        Console.WriteLine("bankBlance is {0}", bankBalance);
     }
 }
