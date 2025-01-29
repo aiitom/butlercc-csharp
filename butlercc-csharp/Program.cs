1d1cdd7d96b3f84e55f3e74d9da9872f597debaa
aiitom <tsays92@yahoo.com>
2025-01-29 02:00:56 -0600

    String exercise using == operator

--- butlercc-csharp/Program.cs
+++ butlercc-csharp/Program.cs
@@ -3,11 +3,11 @@ class DemoVariables
 {
     static void Main()
     {
-        int score = 20;
-        double bankBalance = 345.67;
-        score  = (int)bankBalance + 2; //Was "5" but added a ".2" at the end to see it change from int to double.
-        //Did implicit by adding "(int)" and changing swaping identifiers and changing 5.2 to 2.
-        Console.WriteLine("score is {0}", score);
-        Console.WriteLine("bankBlance is {0}", bankBalance);
+        string name1 = "Amy";
+        string name2 = "Amy";
+        string name3 = "Matthew";
+        Console.WriteLine("compare {0} to {1}: {2}", name1, name2, name1 == name2);
+        Console.WriteLine("compare {0} to {1}: {2}", name1, name3, name1 == name3);
+
     }
 }
