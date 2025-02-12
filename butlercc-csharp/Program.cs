28f893aaadb5312294793910641b58fe99af5edf
aiitom <tsays92@yahoo.com>
2025-02-11 21:16:45 -0600

    Compared three numbers using if else statements to display more than two different WriteLines

--- butlercc-csharp/Program.cs
+++ butlercc-csharp/Program.cs
@@ -1,19 +1,34 @@
 ﻿// Program compares your name to the name of your boss
 using System;
 using static System.Console;
-internal class Program
+class CompareThreeNumbers
 {
     private static void Main(string[] args)
     {
-        const int HIGH = 10;
         string numberString;
-        int number;
+        int num1, num2, num3;
         Write("Enter an interger ");
         numberString = ReadLine();
-        number = Convert.ToInt32(numberString);
-        if (number > HIGH)
-            WriteLine("{0} is greater than {1}", number, HIGH);
+        num1 = Convert.ToInt32(numberString);
+        Write("Enter an interger ");
+        numberString = ReadLine();
+        num2 = Convert.ToInt32(numberString);
+        Write("Enter an interger ");
+        numberString = ReadLine();
+        num3 = Convert.ToInt32(numberString);
+        if (num1 == num2)
+            if (num1 == num3)
+                WriteLine("All three numbers are equal");
+            else
+                WriteLine("First two are equal");
         else
-            WriteLine("{0} is not greater than {1}", number, HIGH);
+                if (num1 == num3)
+            WriteLine("First and last are equal");
+        else
+                if (num2 == num3)
+            WriteLine("Last two are equal");
+        else
+            WriteLine("No two numbers are equal");
+
     }
 }
