790e9b8efb42da7f5098a28b70c10b36c62e2198
aiitom <tsays92@yahoo.com>
2025-02-11 21:30:44 -0600

    Used && operator as an alternative to a nested if statement  that achieved the same result.

--- butlercc-csharp/Program.cs
+++ butlercc-csharp/Program.cs
@@ -1,34 +1,22 @@
 ﻿// Program compares your name to the name of your boss
 using System;
 using static System.Console;
-class CompareThreeNumbers
+internal class Program
 {
     private static void Main(string[] args)
     {
         string numberString;
-        int num1, num2, num3;
-        Write("Enter an interger ");
+        int age;
+        Write("Enter an age number ");
         numberString = ReadLine();
-        num1 = Convert.ToInt32(numberString);
-        Write("Enter an interger ");
-        numberString = ReadLine();
-        num2 = Convert.ToInt32(numberString);
-        Write("Enter an interger ");
-        numberString = ReadLine();
-        num3 = Convert.ToInt32(numberString);
-        if (num1 == num2)
-            if (num1 == num3)
-                WriteLine("All three numbers are equal");
-            else
-                WriteLine("First two are equal");
+        age = Convert.ToInt32(numberString);
+        if (age >= 0 && age < 120)
+            WriteLine("Age is valid");
         else
-                if (num1 == num3)
-            WriteLine("First and last are equal");
-        else
-                if (num2 == num3)
-            WriteLine("Last two are equal");
-        else
-            WriteLine("No two numbers are equal");
+        {
+            WriteLine("Age is invalid");
 
+
+        }
     }
 }
