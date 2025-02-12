d0338ba845751b6cf956d6ee960539923a5b95d0
aiitom <tsays92@yahoo.com>
2025-02-11 21:08:28 -0600

    Create a if-else statement for a number.

--- butlercc-csharp/Program.cs
+++ butlercc-csharp/Program.cs
@@ -1,12 +1,19 @@
 ﻿// Program compares your name to the name of your boss
 using System;
 using static System.Console;
-const int HIGH = 10, LOW = 5;
-string numberString;
-int number;
-Write("Enter an interger ");
-numberString = ReadLine();
-number = Convert.ToInt32(numberString);
-if (number < LOW)
-    if (number < HIGH)
-        WriteLine("{0} is between {1} and {2}", number, LOW, HIGH);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int HIGH = 10;
+        string numberString;
+        int number;
+        Write("Enter an interger ");
+        numberString = ReadLine();
+        number = Convert.ToInt32(numberString);
+        if (number > HIGH)
+            WriteLine("{0} is greater than {1}", number, HIGH);
+        else
+            WriteLine("{0} is not greater than {1}", number, HIGH);
+    }
+}
