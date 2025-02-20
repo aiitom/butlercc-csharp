6219063713a7b82b8f34802c2bd4e7ec38a26eed
aiitom <tsays92@yahoo.com>
2025-02-19 18:10:29 -0600

    Bank balance with interest rate loop with or char for response

--- test-chapter-1/Program.cs
+++ test-chapter-1/Program.cs
@@ -2,20 +2,27 @@
 using static System.Console;
 using System.Globalization;
 using System.Reflection.Metadata;
-class FourHellos
+class LoopingBankBal
 {
     static void Main()
     {
         // Write your code here
-        int number = 1; // starting at 1 then keeps adding +1 til 5 makes 4 hellos, if int number = 0, they would show hello 5 times
-        const int LIMIT = 5;
-        // Limit sets a max number for 4 since int number equals 1
-        //while put the new that will be used from the bottom and the number<limit is bool
-
-        //curly braces surrond the body of the while loop, keep the expected output in that. Make sure to have no ";" in the while() part
-        while (number < LIMIT) {
-            WriteLine("Hello"); 
-            number = number + 1; //Can use number++, ++number or number += 1
+        double bankBal = 1000;
+        const double INT_RATE = 0.04;
+        string inputString;
+        char response;
+        Write("Do you want to see your balance? Y or N...");
+        inputString = ReadLine();
+        response = Convert.ToChar(inputString);
+        while (response != 'Y' || response != 'y')
+        {
+            WriteLine("bankBal balance is {0}",
+                bankBal.ToString("C"));
+            bankBal = bankBal + bankBal * INT_RATE;
+            Write("Do you want to see next year's balance? Y or N...");
+            inputString = ReadLine();
+            response = Convert.ToChar(inputString);
+        }
+        WriteLine("Have a nice day!");
         }
     }
-}
