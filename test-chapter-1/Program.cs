b8ac732a24bdc7bdbcf99981e0cd6a582eafbba3
aiitom <tsays92@yahoo.com>
2025-02-19 20:25:32 -0600

    LoopingBankBal2 with nested loops

--- test-chapter-1/Program.cs
+++ test-chapter-1/Program.cs
@@ -2,26 +2,29 @@
 using static System.Console;
 using System.Globalization;
 using System.Reflection.Metadata;
-class ValidID
+class LoopingBankBal2
 {
     static void Main()
     {
-        // Write your code here
-        //Declare loop control variable and limit
-        int x;
-        const int LIMIT = 10;
-
-        // Using a while loop to display 1 through 10
-        x = 1;
-        while(x <= LIMIT)
+        double bankBal;
+        double rate;
+        int year;
+        const double START_BAL = 1000;
+        const double START_INT = 0.04;
+        const double INT_INCREASE = 0.02;
+        const double LAST_INT = 0.08;
+        const int END_YEAR = 5;
+        for (rate = START_INT; rate <= LAST_INT; rate += INT_INCREASE)
         {
-            WriteLine(x);
-            ++x;
+            bankBal = START_BAL;
+            WriteLine("Starting bank balance is {0}",
+                bankBal.ToString("C"));
+            WriteLine(" Interest Rate: {0}", rate.ToString("P"));
+            for (year = 1; year <= END_YEAR; ++year)
+            {
+                bankBal = bankBal + bankBal * rate;
+                WriteLine(" After year {0}, bank balance is {1}", year, bankBal.ToString("C"));
+            }
         }
-
-        // Using a for loop to display 1 though 10
-        for(x = 1;  x <= LIMIT; ++x)
-            WriteLine(x);
-        }
-    
-        }
+    }
+}
